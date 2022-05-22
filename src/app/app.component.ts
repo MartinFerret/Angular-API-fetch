7b2451aaf3481d18b7f825d4eb38d1367ad09ba5
Martin Ferret <martin@coqli.fr>
2022-05-22 21:58:32 +0100

    hotfixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'project-test';
+export class AppComponent implements OnInit {
+  posts: any;
+  getClick() {
+    var buttons = document.querySelectorAll('button.eliminar');
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i]?.addEventListener('click', function handleClick() {
+        buttons[i].closest('.card')?.classList.add('hide');
+      })
+    }
+  }
+
+  constructor(private service: PostService) { }
+
+  ngOnInit() {
+    this.service.getPosts()
+      .subscribe(response => {
+        this.posts = response;
+        console.log(response);
+      });
+
+  }
 }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private url = "https://api.imgflip.com/get_memes";
+  constructor(private httpClient: HttpClient) { }
+
+  getPosts () {
+    return this.httpClient.get(this.url);
+  }
+}
